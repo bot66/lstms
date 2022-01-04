37394ad441c35aa3e5f293892b7e295b4f8c6874
bot66 <need4data@yeah.net>
2022-01-04 17:08:41 +0800

    add padding_idx=0 in nn.Embedding()

--- networks.py
+++ networks.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 
 
-
 class LSTM(nn.Module):
     def __init__(self,input_size,hidden_size):
         super().__init__()
@@ -14,8 +13,8 @@ class LSTM(nn.Module):
     
     def forward(self,x):
         #input shape [N,L,X]
-        h=torch.zeros((x.shape[0],self.hidden_size),dtype=torch.float32) #[N,hidden_size], init hidden state
-        c=torch.zeros((x.shape[0],self.hidden_size),dtype=torch.float32) #[N,hidden_size], init cell state
+        h=torch.zeros((x.shape[0],self.hidden_size),dtype=torch.float32).to(x.device) #[N,hidden_size], init hidden state
+        c=torch.zeros((x.shape[0],self.hidden_size),dtype=torch.float32).to(x.device) #[N,hidden_size], init cell state
         x=x.permute(1,0,2) #[L,N,X]
         h_states =[] # all timestep hidden states
         for x_i in x:
@@ -27,20 +26,20 @@ class LSTM(nn.Module):
             c=forget_gate.mul(c)+input_gate.mul(candidate)
             h=output_gate.mul(torch.tanh(c))
             h_states.append(h)
-        
+
         return h_states
         
 
 class ManyToOne(nn.Module):
     def __init__(self,vocab_size,input_size,hidden_size,output_size):
         super().__init__()
-        self.embedding=nn.Embedding(vocab_size,input_size)
+        self.embedding=nn.Embedding(vocab_size,input_size,padding_idx=0)
         self.lstm=LSTM(input_size,hidden_size)
-
         self.linear=nn.Linear(hidden_size,output_size)
     def forward(self,x):
         #input shape [N,L,X]
         x=self.embedding(x)
         h=self.lstm(x)
         x=self.linear(h[-1])
+
         return x 
